eccb6afb6bf2af8310bb8a46b2774edbd2454df2
Azin401 <68954976+Azin401@users.noreply.github.com>
2020-11-16 05:41:00 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var bullet,wall;
+var speed,weight;
+
+
+function setup() {
+  createCanvas(1600,400);
+  createSprite(400, 200, 50, 50);
+
+  speed=random(55,90);
+  weight=random(400,1500);
+  bullet=createSprite(50,200,50,50);
+  wall=createSprite(120,200,thickness,height/2);
+
+  
+
+  bullet("white");
+
+var wall,thickness;
+
+var bullet,speed,height;
+
+thickness=random(22,38);
+
+  car.velocityX=speed;
+  
+  speed=random(223,321);
+  weight=random(30,52);
+}
+
+function draw() {
+  background(255,255,255); 
+  wall=createSprite(1500,200,60,height/2);
+  color(80,80,80); 
+  drawSprites();
+}
+
+
